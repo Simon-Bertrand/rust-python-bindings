674e484ad46e25e729f021ea3f18def1ee758e82
Simon Bertrand <84455908+Simon-Bertrand@users.noreply.github.com>
2024-02-01 20:26:48 +0100

    added numpy template

---
+++ numpy-template/python/numpy_template/__init__.py
@@ -0,0 +1,3 @@
+from .numpy_lib import eye  # export public parts of the binary extension
+
+__all__ = ["eye"]
---
+++ numpy-template/src/lib.rs
@@ -0,0 +1,17 @@
+use std::ops::Add;
+use ndarray;
+use pyo3::prelude::*;
+use numpy::{PyArray2, IntoPyArray};
+
+#[pymodule]
+fn numpy_lib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    #[pyfn(m)]
+    #[pyo3(name = "eye")]
+    fn eye<'py>(py: Python<'py>, size: usize) -> &PyArray2<f64> {
+        let array = ndarray::Array::eye(size);
+        array.into_pyarray(py)
+    }
+    Ok(())
+}
+
+ 
